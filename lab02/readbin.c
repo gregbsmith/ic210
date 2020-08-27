87078b41d9955745244df1307bb13c72ed00cd4a
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-08-27 10:55:00 -0400

    msg

--- lab02/readbin.c
+++ lab02/readbin.c
@@ -1,3 +1,40 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
+/* Gregory Smith
+ * 235952
+ * 
  */
+
+#include "ic210.h"
+
+int main() {
+	
+	char first = '0';
+	char second = '0';
+	char third = '0';
+	char fourth = '0';
+	int total = 0;
+	
+	fputs("Enter a 4-bit binary number: ", stdout);
+	
+	first = readchar(stdin);
+	second = readchar(stdin);
+	third = readchar(stdin);
+	fourth = readchar(stdin);
+	
+	int fi = first - 48;
+	int s = second - 48;
+	int t = third - 48;
+	int fo = fourth - 48;
+	
+	total = (8 * fi) + (4 * s) + (2 * t) + (fo);
+	
+	fputs("In decimal ", stdout);
+	writenum(fi, stdout);
+	writenum(s, stdout);
+	writenum(t, stdout);
+	writenum(fo, stdout);
+	fputs(" = ", stdout);
+	writenum(total, stdout);
+	
+	fputs("\n", stdout);
+	return 0;
+}
--- lab02/writebin.c
+++ lab02/writebin.c
@@ -1,3 +1,40 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
+/* Gregory Smith
+ * 235952
  */
+
+#include "ic210.h"
+
+int main() {
+	
+	int input = 0;
+	int b1 = 0;
+	int b2 = 0;
+	int b3 = 0;
+	int b4 = 0;
+	
+	fputs("Enter a number between 0 and 15: ", stdout);
+	input = readnum(stdin);
+	int input_save = input;
+	
+	b1 = input / 8;
+	input = input % 8;
+	
+	b2 = input / 4;
+	input = input % 4;
+	
+	b3 = input / 2;
+	input = input % 2;
+	
+	b4 = input;
+	
+	writenum(input_save, stdout);
+	fputs(" in binary is ", stdout);
+	writenum(b1, stdout);
+	writenum(b2, stdout);
+	writenum(b3, stdout);
+	writenum(b4, stdout);
+	
+	
+	fputs("\n", stdout);
+	return 0;
+}
