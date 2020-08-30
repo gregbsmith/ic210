3fd303059b2ff6d2105d6e0cc9872764d5f9e6bc
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-08-30 17:45:49 -0400

    msg

--- hw05/sleeping.c
+++ hw05/sleeping.c
@@ -1,3 +1,18 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
+/* Gregory Smith
+ * 235952
  */
+
+#include "ic210.h"
+
+int main() {
+	
+	fputs("Time: ", stdout);
+	int time = readnum(stdin);
+	if(time >= 600 && time <=2300){
+		fputs("Awake", stdout);
+	}else{
+		fputs("Asleep", stdout);
+	}
+	fputs("\n", stdout);
+	return 0;
+}
