24c90abdb8b301fe97a79deba2e75005778e05a2
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-08-30 17:39:35 -0400

    msg

--- hw05/capitalize.c
+++ hw05/capitalize.c
@@ -1,3 +1,23 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
+/* Gregory Smith
+ * 235952
  */
+
+#include "ic210.h"
+
+int main() {
+	
+	fputs("Input word: ", stdout);
+	char first = readchar(stdin);
+	first = first - 32;
+	cstring theRest;
+	readstring(theRest, stdin);
+	
+	
+	
+	
+	fputs("Capitalized: ", stdout);
+	fputc(first, stdout);
+	fputs(theRest, stdout);
+	fputs("\n", stdout);
+	return 0;
+}
