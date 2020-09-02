6f62d94ad391d311e07e424aa28ff01cd12c0e52
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-09-01 20:02:55 -0400

    msg

--- hw06/userpass.c
+++ hw06/userpass.c
@@ -1,3 +1,33 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
+/* Gregory Smith
+ * 235952
  */
+
+#include "ic210.h"
+
+int main(){
+	
+	cstring YES = "yes";
+	cstring NO = "no";
+	
+	cstring SUPE = "supe";
+	cstring BEATARMY = "beatarmy";
+	cstring ROCHE = "roche";
+	cstring EMACS = "iloveemacs";
+	
+	cstring uName;
+	cstring uPass;
+	
+	fputs("Username: ", stdout);
+	readstring(uName, stdin);
+	fputs("Password: ", stdout);
+	readstring(uPass, stdin);
+	
+	if((stringeq(uName, SUPE) && stringeq(uPass, BEATARMY)) || (stringeq(uName, ROCHE) && stringeq(uPass, EMACS))){
+		fputs(YES, stdout);
+	}else{
+		fputs(NO, stdout);
+	}
+	
+	fputs("\n", stdout);
+	return 0;
+}
