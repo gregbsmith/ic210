73b0bd2b1e1997f198f6da707cdf170b12f267a6
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-09-03 15:32:31 -0400

    msg

--- hw07/leap.c
+++ hw07/leap.c
@@ -6,21 +6,28 @@
 
 int main(){
 	int year = 0;
+	bool leap = 0;
+	//fputs("What is your favorite leap year? ", stdout);
+	//year = readnum(stdin);
 	
-	fputs("What is your favorite leap year? ", stdout);
-	year = readnum(stdin);
-	
-	if(((year % 4 == 0) && (year % 100 != 0)) ||
-		(year % 400 == 0))
+	while(!leap)
 	{
-		fputs("Yes, ", stdout);
-		writenum(year, stdout);
-		fputs(" was a good year.", stdout);
-	}
-	else
-	{
-		writenum(year, stdout);
-		fputs(" is not a leap year!", stdout);
+		fputs("What is your favorite leap year? ", stdout);
+		year = readnum(stdin);
+		
+		if(((year % 4 == 0) && (year % 100 != 0)) ||
+			(year % 400 == 0))
+		{
+			fputs("Yes, ", stdout);
+			writenum(year, stdout);
+			fputs(" was a good year.", stdout);
+			leap = true;
+		}
+		else
+		{
+			writenum(year, stdout);
+			fputs(" is not a leap year!\n", stdout);
+		}
 	}
 	
 	fputs("\n", stdout);
