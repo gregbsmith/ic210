cba0fa401ffed367e05351797d8531e010eb32a9
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-09-17 10:04:26 -0400

    msg

--- proj1/gray.c
+++ proj1/gray.c
@@ -1,3 +1,25 @@
 /* gray.c: Program to convert a ppm file to grayscale
- * YOUR NAME HERE
+ * Gregory Smith
  */
+
+#include "ic210.h"
+
+int main(){
+	cstring inFile;
+	cstring outFile;
+	fputs("Input file: ", stdout);
+	readstring(inFile, stdin);
+	fputs("Output file: ", stdout);
+	readstring(outFile, stdin);
+	
+	//stream proj = fopen("proj01files.tgz", "r");
+	
+	stream fin = fopen(inFile, "r");
+	if(!fin){
+		//error
+		return 1;
+	}
+	
+	fputs("\n", stdout);
+	return 0;
+}
