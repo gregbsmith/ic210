8520435e429cc562d79a409d6b10f3a0136fe6a5
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-09-20 22:34:57 -0400

    msg

--- proj1/README.txt
+++ proj1/README.txt
@@ -4,7 +4,7 @@ Any help you got from other people:
 
 
 Any websites you used (besides the course website and cppreference.com):
-
+	I used guru99 to read more about streams and FILE*
 
 What did you think of this project?
 (Too easy, too hard, confusing, awesome, mind-blowing, boring, ...?)
--- proj1/gray.c
+++ proj1/gray.c
@@ -7,6 +7,7 @@
 int main(){
 	cstring inFile;
 	cstring outFile;
+	cstring trash1;
 	fputs("Input file: ", stdout);
 	readstring(inFile, stdin);
 	fputs("Output file: ", stdout);
@@ -17,9 +18,47 @@ int main(){
 	stream fin = fopen(inFile, "r");
 	if(!fin){
 		//error
+		fputs("Error: Input file not found", stdout);
 		return 1;
 	}
 	
+	FILE *fp;
+	fp = fopen(outFile, "w");
+	
+	int width = 0;
+	int height = 0;
+	int high = 0;
+	
+	fscanf(fin, "%s %d %d %d", trash1, &width, &height, &high);
+	
+	fprintf(fp, "%s\n%d %d\n%d\n", trash1, width, height, high);
+	
+	
+	for(int i = 0; i < height; i++){
+		for(int j = 0; j < width; j++){
+			int red = 0;
+			int green = 0;
+			int blue = 0;
+			fscanf(fin, "%i %i %i", &red, &green, &blue);
+			red = red + green + blue;
+			red = red / 3;
+			green = red;
+			blue = red;
+			fprintf(fp, "%i %i %i", red, green, blue);
+			
+			if(width - 1 == j){
+				fprintf(fp, "\n");
+			}else{
+				fprintf(fp, " ");
+			}
+			//fprintf(stdout, "row %i, col %i: r%i g%i b%i\n", i, j, red, green, blue);
+		}
+	}
+	
+	fprintf(stdout, "Image saved to %s", outFile);
+	
+	fclose(fp);
+	fclose(fin);
 	fputs("\n", stdout);
 	return 0;
 }
