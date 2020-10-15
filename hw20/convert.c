5aebe2d814eacc456e0038816f4a947103e8ad32
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-10-14 20:22:56 -0400

    msg

--- hw20/convert.c
+++ hw20/convert.c
@@ -1,3 +1,46 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
+/* Gregory Smith
  */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+typedef char cstring[128];
+
+int main(){
+	cstring read;
+	cstring convstr;
+	int most = 0;
+	scanf("%s", read);
+	strcpy(convstr, read);
+	while(strcmp(read, "DONE") != 0){
+		int len = strlen(read);
+		int conv = 0;
+		for(int i = 0; i < len; i++){
+			if(read[i] == 'a'){
+				read[i] = '@';
+				conv++;
+			}else if(read[i] == 'e'){
+				read[i] = '3';
+				conv++;
+			}else if(read[i] == 'i'){
+				read[i] = '!';
+				conv++;
+			}else if(read[i] == 't'){
+				read[i] = '+';
+				conv++;
+			}
+			
+		}
+		
+		if(conv > most){
+			most = conv;
+			strcpy(convstr, read);
+		}
+		scanf("%s", read);
+	}
+	
+	printf("%d conversions for \"%s\"\n", most, convstr);
+}
