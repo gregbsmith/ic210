db0aef956a71babc6645c1755874a0f88f1b5d1b
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-10-22 08:31:50 -0400

    msg

--- hw22/pairs.c
+++ hw22/pairs.c
@@ -1,4 +1,35 @@
 /* pairs.c: Read in x and y values and pair them up.
- * Erase this comment and put your name here!
- * Your actual code should start below.
+ * Gregory Smith
  */
+
+#include "stdio.h"
+#include "string.h"
+#include "stdbool.h"
+
+typedef char cstring[128];
+
+int main(){
+	int N = 0;
+	printf("N: ");
+	scanf("%d", &N);
+	double x[N];
+	double y[N];
+	
+	printf("x values: ");
+	for(int i = 0; i < N; i++){
+		scanf("%lg", &x[i]);
+	}
+	
+	printf("y values: ");
+	for(int i = 0; i < N; i++){
+		scanf("%lg", &y[i]);
+	}
+	
+	for(int i = 0; i < N; i++){
+		printf("(%lg,%lg) ", x[i], y[i]);
+	}
+	
+	printf("\n");
+	
+	return 0;
+}
--- hw22/substring.c
+++ hw22/substring.c
@@ -1,4 +1,41 @@
 /* substring.c: Read two strings and determine whether one contains the other.
- * Erase this comment and put your name here!
- * Your actual code should start below.
+ * Gregory Smith
  */
+#include "string.h"
+#include "stdio.h"
+#include "stdbool.h"
+typedef char cstring[128];
+
+bool match(cstring target, cstring test, int offset);
+
+int main(){
+	bool have_match = false;
+	printf("Target: ");
+	cstring target;
+	scanf("%s", target);
+	printf("Test: ");
+	cstring test;
+	scanf("%s", test);
+	
+	for(int i = 0; i < strlen(test); i++){
+		if(test[i] == target[0] && strlen(test) - i >= strlen(target)){
+			have_match = match(target, test, i);
+		}
+		if(have_match){
+			printf("yes\n");
+			return 0;
+		}
+	}
+	printf("no\n");
+	
+	return 0;
+}
+
+bool match(cstring target, cstring test, int offset){
+	for(int i = 0; i < strlen(target); i++){
+		if(target[i] != test[offset + i]){
+			return false;
+		}
+	}
+	return true;
+}
