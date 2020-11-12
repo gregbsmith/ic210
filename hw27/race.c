4bd3864e684afb89a81bb23617b1774aa97b41fe
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-11-12 10:04:50 -0500

    msg

--- hw27/race.c
+++ hw27/race.c
@@ -1,3 +1,34 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
+/* Gregory Smith
  */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+
+typedef char cstring[128];
+
+struct athlete {
+	cstring name;
+	double time;
+};
+	
+
+int main() {
+	printf("Enter the names and times of two racers:\n");
+	struct athlete a;
+	struct athlete b;
+	
+	scanf("%s %lg\n", a.name, &a.time);
+	scanf("%s %lg", b.name, &b.time);
+	
+	if(a.time < b.time) {
+		printf("%s won with a time of %lg minutes.\n", a.name, a.time);
+	}else {
+		printf("%s won with a time of %lg minutes.\n", b.name, b.time);
+	}
+	
+	return 0;
+}
