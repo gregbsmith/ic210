8122aada6646d30966af10f94f6691a372610773
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-11-16 22:23:50 -0500

    msg

--- hw29/raffle.c
+++ hw29/raffle.c
@@ -1,3 +1,57 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
+/* Gregory Smith
  */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef char cstring[128];
+
+typedef struct {
+	cstring name;
+	int tickets[4];
+} Person;
+
+int main() {
+	printf("How many people? ");
+	int ppl = 0;
+	scanf(" %d", &ppl);
+	Person People[ppl];
+	for(int i = 0; i < ppl; i++) {
+		printf("Person %d name: ", i + 1);
+		scanf(" %s", People[i].name);
+		printf("Person %d tickets: ", i + 1);
+		for(int j = 0; j < 4; j++) {
+			scanf(" %d", &People[i].tickets[j]);
+		}
+	}
+	int winticket = 0;
+	printf("Winning ticket: ");
+	scanf(" %d", &winticket);
+	
+	int winners[5];
+	
+	for(int i = 0; i < 5; i++) {
+		winners[i]= -1;
+	}
+	
+	int windex = 0;
+	for(int i = 0; i < ppl; i++) {
+		for(int j = 0; j < 4; j++) {
+			if(People[i].tickets[j] == winticket) {
+				winners[windex] = i;
+				windex++;
+			}
+		}
+	}
+	
+	for(int i = 0; i < 4; i++) {
+		if(winners[i] != -1 && winners[i] != winners[i+1]) {
+			printf("%s won!\n", People[winners[i]].name);
+		}
+	}
+	
+	return 0;
+}
