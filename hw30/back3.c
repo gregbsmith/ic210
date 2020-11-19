13a746608ca55d241b82c8aa1ab4e699a71c50b8
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-11-19 10:13:08 -0500

    msg

--- hw30/back3.c
+++ hw30/back3.c
@@ -1,3 +1,34 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
+/* Gregory Smith
  */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct node {
+	int data;
+	struct node* next;
+};
+
+struct node* add2front(int val, struct node* oldlist);
+
+int main() {
+	int temp;
+	
+	struct node* list = NULL;
+	
+	while(scanf(" %d", &temp) && temp != -1) {
+		list = add2front(temp, list);
+	}
+	
+	printf("%d\n", list->next->next->data);
+	
+	return 0;
+}
+
+struct node* add2front(int val, struct node* oldlist) {
+  struct node* temp = calloc(1, sizeof(struct node));
+  temp->data = val;
+  temp->next = oldlist;
+  return temp;
+}
