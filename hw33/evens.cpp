5346fb303eb7178f5f81c22d14030cfb6ec42fdf
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-11-29 13:54:59 -0500

    msg

--- hw33/evens.cpp
+++ hw33/evens.cpp
@@ -1,3 +1,42 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
+/* Gregory Smith
+ * counts the even numbers
  */
+
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int* readnums(int& size);
+bool isEven(int x);
+
+int main() {
+	int n;
+	int* nums = readnums(n);
+	
+	int count = 0;
+	for(int i = 0; i < n; i++) {
+		if(isEven(nums[i])) {
+			count++;
+		}
+	}
+	printf("%d even numbers\n", count);
+	delete [] nums;
+	return 0;
+}
+
+int* readnums(int& size) {
+	printf("How many numbers? ");
+	scanf(" %d", &size);
+	
+	int* arr = new int[size];
+	
+	for(int i = 0; i < size; i++) {
+		scanf(" %d", &arr[i]);
+	}
+	
+	return arr;
+}
+
+bool isEven(int x) {
+	return x % 2 == 0;
+}
