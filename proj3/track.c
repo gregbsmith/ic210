b7e4d6d38da0225bb4b40e8b1fd13cd7debc9913
Gregory Smith <m235952@usna.edum235952@usna.edu>
2020-11-30 16:07:26 -0500

    msg

--- proj3/track.c
+++ proj3/track.c
@@ -73,8 +73,49 @@ int main() {
 		
 		// printf("\nshould say trkseg: %s\n", (char*)trkseg->name);
 		
+		xmlNode* trkpt = trkseg->children->next;
 		
+		while(trkpt != NULL && !strcmp("trkpt", (char*)trkpt->name)) {
+			xmlAttr* lat = trkpt->properties;
+			xmlAttr* lon = trkpt->properties->next;
+			// printf("\n%s %s \n", (char*)lat->children->content, (char*)lon->children->content);
+			cstring lat_str;
+			strcpy(lat_str, (char*)lat->children->content);
+			cstring lon_str;
+			strcpy(lon_str, (char*)lon->children->content);
+			double latitude = 0;
+			double longitude = 0;
+			sscanf(lat_str, "%lg", &latitude);
+			sscanf(lon_str, "%lg", &longitude);
+			// printf("\n%lg\n%lg\n", latitude, longitude);
+			
+			xmlNode* x_time = trkpt->children->next;
+			while(strcmp("time", (char*)x_time->name)) {
+				x_time = x_time->next;
+			}
+			// printf("\nshould say time: %s\n", (char*)x_time->name);
+			cstring time_str = "";
+			strcpy(time_str, (char*)x_time->children->content);
+			//printf("\n%s\n", time_str);
+			
+			Waypoint w_cur;
+			struct tm tm_cur;
+			
+			// 2016-09-16T20:01:03Z
+			strptime(time_str, "%Y-%m-%eT%H:%M:%SZ", &tm_cur);
+			tm_cur.tm_isdst = 0;
+			
+			w_cur.time = mktime(&tm_cur);
+			// printf("\n%d\n", (int)w_cur.time);
+			w_cur.coor = make_gps(latitude, longitude);
+			L = add2front(w_cur, L);
+			
+			
+			trkpt = trkpt->next->next;
+			num_points++;
+		}
 		
+		printf("%d waypoints\n", num_points);
 		xmlFreeDoc(fin);
 	}else {
 		
@@ -109,6 +150,7 @@ int main() {
 		}
 		
 		fclose(fin);
+		// printf("%d %d\n", (int)L->data.time, (int)L->next->data.time);
 	}
 	
 	cstring command;
